776c21c948abb3f4d363a774fb8c4d8ac5bda3be
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-05-02 17:32:27 +0900

    header 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,36 @@
+import { SiVisualstudiocode } from "react-icons/si";
+import { BsFillCircleFill } from "react-icons/bs";
+import styles from "./Header.module.css";
+
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <ul className={styles.menu}>
+        <SiVisualstudiocode className={styles.vscodeIcon} />
+        <li>File</li>
+        <li>Edit</li>
+        <li>View</li>
+        <li>Go</li>
+        <li>Run</li>
+        <li>Terminal</li>
+        <li>Help</li>
+      </ul>
+
+      <p>Jonghwan Jang - Visual Studio Code</p>
+
+      <ul className={styles.circle}>
+        <li>
+          <BsFillCircleFill />
+        </li>
+        <li>
+          <BsFillCircleFill />
+        </li>
+        <li>
+          <BsFillCircleFill />
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
