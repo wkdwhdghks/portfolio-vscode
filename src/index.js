910334b020ef2d70381366aa50d45c1b6b7eb1eb
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-05-13 02:10:48 +0900

    배포

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
 import { ThemeProvider } from "./context/ThemeContext";
-import { HashRouter } from "react-router-dom";
 import "./styles/reset.css";
 import "./styles/global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
-    <ThemeProvider>
-      <Router />
-    </ThemeProvider>
-  </HashRouter>
+  <ThemeProvider>
+    <Router />
+  </ThemeProvider>
 );
