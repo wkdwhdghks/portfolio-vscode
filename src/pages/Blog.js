9df6b4cdcc00e9cdb3629dc3c3046f14137bf4d5
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-05-12 23:13:08 +0900

    402에러 오류 찾기

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,35 +1,37 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Note from "../components/Note";
 import styles from "./Blog.module.css";
 
-function Blog() {
+const Blog = () => {
   const [post, setPost] = useState([]);
   const [bgUrl] = useState([]);
   const encoded = encodeURIComponent("https://wkdwhdghks.tistory.com/rss");
-  const API_KEY = process.env.REACT_APP_RSSTOJSON_API_KEY;
+  const apiKey = process.env.REACT_APP_RSSTOJSON_API_KEY;
+
+  const getData = async () => {
+    try {
+      const result = await axios.get(
+        `https://api.rss2json.com/v1/api.json?rss_url=${encoded}&api_key=${apiKey}`
+      );
+      console.log("Get data!");
+      setPost(result.data.items);
+      result.data.items.map((item) => {
+        const desc = item.description;
+        const url = desc.slice(
+          desc.indexOf('data-url="') + 10,
+          desc.indexOf('" data-lightbox=')
+        );
+        return bgUrl.push(`${url}`);
+      });
+    } catch (error) {
+      console.log("Data load failed:" + error);
+    }
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const result = await axios.get(
-          `https://api.rss2json.com/v1/api.json?rss_url=${encoded}&api_key=${API_KEY}`
-        );
-        setPost(result.data.items);
-        result.data.items.map((item) => {
-          const desc = item.description;
-          const url = desc.slice(
-            desc.indexOf('data-url="') + 10,
-            desc.indexOf('" data-lightbox=')
-          );
-          return bgUrl.push(`${url}`);
-        });
-      } catch (error) {
-        console.log("Data load failed:" + error);
-      }
-    };
     getData();
-  }, [API_KEY, bgUrl, encoded]);
+  }, []);
 
   return (
     <div>
@@ -54,6 +56,6 @@ function Blog() {
       </div>
     </div>
   );
-}
+};
 
 export default Blog;
